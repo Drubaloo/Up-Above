e5309ca4288a9c43c09de4840ae24301fbab3583
Kayla Ward <kaylaw0127@gmail.com>
2020-02-13 19:55:45 -0800

    removed divs that are no longer needed

--- assets/js/planetapi.js
+++ assets/js/planetapi.js
@@ -8,29 +8,6 @@
 // 243: Earth
 // 244: Venus
 
-// moon variables
-// var moon1 = $("#mercury-moon")
-// var moon2 = $("#venus-moon")
-// var moon3 = $("#earth-moon")
-// var moon4 = $("#mars-moon")
-// var moon5 = $("#jupiter-moon")
-// var moon6 = $("#saturn-moon")
-// var moon7 = $("#uranus-moon")
-// var moon8 = $("#neptune-moon")
-// var moon9 = $("#pluto-moon")
-
-// discovered variables
-// var mercuryDiscovered = $("#mercury-discovered")
-// var venusDiscovered = $("#venus-discovered")
-// var earthDiscovered = $("#earth-discovered")
-// var marsDiscovered = $("#mars-discovered")
-// var jupiterDiscovered = $("#jupiter-discovered")
-// var saturnDiscovered = $("#saturn-discovered")
-// var uranusDiscovered = $("#uranus-discovered")
-// var neptuneDiscovered = $("#neptune-discovered")
-// var plutoDiscovered = $("#pluto-discovered")
-
-
 var planets = [240, 244, 243, 239, 238, 241, 199, 219, 208]
 var queryURL = "https://api.le-systeme-solaire.net/rest/bodies/"
 $.ajax({
@@ -38,9 +15,8 @@ $.ajax({
     method: "GET"
 
 }).then(function (response) {
-    console.log(response.bodies[planets[1]])
-    console.log(response.bodies[244])
 
+    // moon length
     for (var i = 0; i < planets.length; i++) {
         if (!response.bodies[planets[i]].moons) {
             $(`#${response.bodies[planets[i]].id}-moon`).text("Moons: 0")
@@ -48,4 +24,5 @@ $.ajax({
             $(`#${response.bodies[planets[i]].id}-moon`).text("Moons: " + response.bodies[planets[i]].moons.length)
         }
     }
+
 })
